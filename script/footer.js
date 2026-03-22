cc578ca35dc61e7d2ba884256215738f97655b47
Etherix-Casual <etherix.15@gmail.com>
2026-03-22 18:46:52 -0300

    update xxx

--- script/footer.js
+++ script/footer.js
@@ -1,8 +1,8 @@
 const footerHTML = `
     <footer class="rodape">
         <p>Feito com ❤️... e muito café ☕☕☕☕☕ tipo... muito mesmo</p>
-        <p>Tambem começei a usar IA, muito bom pra agilizar as coisas, mas ainda tem muito trabalho manual envolvido</p>
-        <p>nota: todas as questões de quizz foram criadas por IA, então se encontrar algum erro ou algo estranho, por favor me avise</p>
+        <p>Quero 20 cafés por dia, por favor! ☕☕☕☕☕</p>
+        <p>sem café, sem código! ☕💻</p>
         <p>Yonia - 2026©</p>
         <p>versão 1.5</p>
     </footer>
